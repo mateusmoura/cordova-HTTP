9972344cb78d8faa09a137ec8822a5775aa430c1
Mateus Moura <chagas.mateus@gmail.com>
2017-04-24 15:48:26 -0300

    :bug: fix window.cordovaHTTP

--- www/cordovaHTTP.js
+++ www/cordovaHTTP.js
@@ -101,75 +101,75 @@ var http = {
 };
 
 module.exports = http;
-
-if (typeof angular !== "undefined") {
-    angular.module('cordovaHTTP', []).factory('cordovaHTTP', function($timeout, $q) {
-        function makePromise(fn, args, async) {
-            var deferred = $q.defer();
+window.cordovaHTTP = http;
+// if (typeof angular !== "undefined") {
+//     angular.module('cordovaHTTP', []).factory('cordovaHTTP', function($timeout, $q) {
+//         function makePromise(fn, args, async) {
+//             var deferred = $q.defer();
             
-            var success = function(response) {
-                if (async) {
-                    $timeout(function() {
-                        deferred.resolve(response);
-                    });
-                } else {
-                    deferred.resolve(response);
-                }
-            };
+//             var success = function(response) {
+//                 if (async) {
+//                     $timeout(function() {
+//                         deferred.resolve(response);
+//                     });
+//                 } else {
+//                     deferred.resolve(response);
+//                 }
+//             };
             
-            var fail = function(response) {
-                if (async) {
-                    $timeout(function() {
-                        deferred.reject(response);
-                    });
-                } else {
-                    deferred.reject(response);
-                }
-            };
+//             var fail = function(response) {
+//                 if (async) {
+//                     $timeout(function() {
+//                         deferred.reject(response);
+//                     });
+//                 } else {
+//                     deferred.reject(response);
+//                 }
+//             };
             
-            args.push(success);
-            args.push(fail);
+//             args.push(success);
+//             args.push(fail);
             
-            fn.apply(http, args);
+//             fn.apply(http, args);
             
-            return deferred.promise;
-        }
+//             return deferred.promise;
+//         }
         
-        var cordovaHTTP = {
-            getBasicAuthHeader: http.getBasicAuthHeader,
-            useBasicAuth: function(username, password) {
-                return http.useBasicAuth(username, password);
-            },
-            setHeader: function(header, value) {
-                return http.setHeader(header, value);
-            },
-            enableSSLPinning: function(enable) {
-                return makePromise(http.enableSSLPinning, [enable]);
-            },
-            acceptAllCerts: function(allow) {
-                return makePromise(http.acceptAllCerts, [allow]);
-            },
-            validateDomainName: function(validate) {
-                return makePromise(http.validateDomainName, [validate]);
-            },
-            post: function(url, params, headers) {
-                return makePromise(http.post, [url, params, headers], true);
-            },
-            get: function(url, params, headers) {
-                return makePromise(http.get, [url, params, headers], true);
-            },
-            head: function(url, params, headers) {
-                return makePromise(http.head, [url, params, headers], true);
-            },
-            uploadFile: function(url, params, headers, filePath, name) {
-                return makePromise(http.uploadFile, [url, params, headers, filePath, name], true);
-            },
-            downloadFile: function(url, params, headers, filePath) {
-                return makePromise(http.downloadFile, [url, params, headers, filePath], true);
-            }
-        };
-        return cordovaHTTP;
-    });
-} else {
-    window.cordovaHTTP = http;
-}
+//         var cordovaHTTP = {
+//             getBasicAuthHeader: http.getBasicAuthHeader,
+//             useBasicAuth: function(username, password) {
+//                 return http.useBasicAuth(username, password);
+//             },
+//             setHeader: function(header, value) {
+//                 return http.setHeader(header, value);
+//             },
+//             enableSSLPinning: function(enable) {
+//                 return makePromise(http.enableSSLPinning, [enable]);
+//             },
+//             acceptAllCerts: function(allow) {
+//                 return makePromise(http.acceptAllCerts, [allow]);
+//             },
+//             validateDomainName: function(validate) {
+//                 return makePromise(http.validateDomainName, [validate]);
+//             },
+//             post: function(url, params, headers) {
+//                 return makePromise(http.post, [url, params, headers], true);
+//             },
+//             get: function(url, params, headers) {
+//                 return makePromise(http.get, [url, params, headers], true);
+//             },
+//             head: function(url, params, headers) {
+//                 return makePromise(http.head, [url, params, headers], true);
+//             },
+//             uploadFile: function(url, params, headers, filePath, name) {
+//                 return makePromise(http.uploadFile, [url, params, headers, filePath, name], true);
+//             },
+//             downloadFile: function(url, params, headers, filePath) {
+//                 return makePromise(http.downloadFile, [url, params, headers, filePath], true);
+//             }
+//         };
+//         return cordovaHTTP;
+//     });
+// } else {
+//     window.cordovaHTTP = http;
+// }
